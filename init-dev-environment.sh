d50aa7d369cef8047045f435c27a9064375fe17d
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-01-14 15:19:12 +0100

    Add resources

--- init-dev-environment.sh
+++ init-dev-environment.sh
@@ -1,23 +1,9 @@
-#!/usr/bin/env bash
-#
-# Copyright 2019 Project OpenUBL, Inc. and/or its affiliates
-# and other contributors as indicated by the @author tags.
-#
-# Licensed under the Eclipse Public License - v 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.eclipse.org/legal/epl-2.0/
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
-wget https://github.com/keycloak/keycloak/releases/download/12.0.1/keycloak-12.0.1.tar.gz -P workload/
-tar xvzf workload/keycloak-*.tar.gz -C workload/
+#!/usr/bin/env bash
+
+KcVersion=12.0.1
+wget https://github.com/keycloak/keycloak/releases/download/$KcVersion/keycloak-$KcVersion.tar.gz -P workspace/
+tar xvzf workspace/keycloak-*.tar.gz -C workspace/
 
 xmlstarlet ed \
 -L \
@@ -27,6 +13,8 @@ xmlstarlet ed \
 -u '/s:server/s:profile/su:subsystem/su:theme/su:cacheThemes' -v "false" \
 -u '/s:server/s:profile/su:subsystem/su:theme/su:cacheTemplates' -v "false" \
 -u '/s:server/s:profile/su:subsystem/su:theme/su:dir' -v "$(pwd)/src/main/resources/themes" \
-workload/keycloak-*/standalone/configuration/standalone.xml
+workspace/keycloak-*/standalone/configuration/standalone.xml
 
-cp -r workload/keycloak-*/themes/* src/main/resources/themes/
+cp -r workspace/keycloak-*/themes/* src/main/resources/themes/
+
+echo "You can start the server using ./workspace/keycloak-$KcVersion/bin/standalone.sh"
